8bda9e1ed6e231ea89a600a2260ab1a5822d869e
Fernanda Mendes <89420917+fernandamgomes@users.noreply.github.com>
2021-08-31 15:48:12 -0300

    Create mario-more.py

---
+++ pset6/mario-more.py
@@ -0,0 +1,12 @@
+altura = 0 
+while ((altura > 8) or (altura < 1)):
+    # Input do usuário 
+    altura = int(input("Height: "))
+    
+
+for linha in range(altura):
+    # Início da linha
+    print((altura - linha - 1) * " ", end="")
+    print((linha + 1) * "#", end="  ")
+    print((linha + 1) * "#")
+    # Fim da linha
