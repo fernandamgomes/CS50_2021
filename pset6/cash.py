21a6fe145aa7186efb1acfd6df907c1496ccde2a
Fernanda Mendes <89420917+fernandamgomes@users.noreply.github.com>
2021-08-31 15:44:55 -0300

    Create cash.py

---
+++ pset6/cash.py
@@ -0,0 +1,29 @@
+# Input positivo do usuário
+dolar = 0
+while (dolar <=0):
+    dolar = float(input("Change owed: "))
+
+# Dolar -> centavo
+centavos = round(dolar * 100)
+
+soma = 0
+
+# Checar cada tipo de moeda
+while centavos >= 25:
+    soma = soma + 1
+    centavos = centavos - 25
+
+while centavos >= 10:
+    soma = soma + 1
+    centavos = centavos - 10
+
+while centavos >= 5:
+    soma = soma + 1
+    centavos = centavos - 5
+
+while centavos >= 1:
+    soma = soma + 1
+    centavos = centavos - 1
+
+# Imprimir total de moedas
+print(soma)
