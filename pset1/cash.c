604e7e9a3c1f2b748fe0d03ccdb2f0ccfa14b864
Fernanda Mendes <89420917+fernandamgomes@users.noreply.github.com>
2021-08-31 12:42:10 -0300

    Create cash.c

---
+++ pset1/cash.c
@@ -0,0 +1,51 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+// program that first asks the user how much change is owed and then prints the minimum number of coins with which that change can be made
+
+int main(void)
+{
+    //VARIÁVEIS
+    float dolar;
+    int soma = 0;
+
+
+    //PEDIR VALOR DO TROCO
+    do
+    {
+        dolar = get_float("Change:");
+    }
+    while (dolar < 0);
+
+    int centavos = round(dolar * 100);
+
+    //CALCULAR NÚMERO DE MOEDAS
+    while (centavos >= 25)
+    {
+        soma = soma + 1;
+        centavos = centavos - 25;
+    }
+
+    while (centavos >= 10)
+    {
+        soma = soma + 1;
+        centavos = centavos - 10;
+    }
+
+    while (centavos >= 5)
+    {
+        soma = soma + 1;
+        centavos = centavos - 5;
+    }
+
+    while (centavos >= 1)
+    {
+        soma = soma + 1;
+        centavos = centavos - 1;
+    }
+
+    //IMPRIMIR NÚMERO DE MOEDAS
+    printf("%i\n", soma);
+}
